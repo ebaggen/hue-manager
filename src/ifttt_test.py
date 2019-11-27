383039ada1d0751f6049b13cf9584ea2e80efd5b
Eric Baggen <eric.baggen@gmail.com>
2019-11-27 01:19:37 -0800

    Migrated to PubSub paradigm as an MQTT client

--- src/ifttt_test.py
+++ src/ifttt_test.py
@@ -19,28 +19,43 @@ except Adafruit_IO.RequestError:
 bridge = phue.Bridge(config['hue']['ip_address'])
 lights = bridge.get_light_objects()
 
-def acknowledge_adafruit_event():
-    # Acknowledge event by resetting it
-    adafruit_io.send(ifttt_feed.key, 0)
+def connected(client):
+    print('Connected to Adafruit IO.')
+    client.subscribe(config['adafruit_io']['feed_id'], config['adafruit_io']['username'])
 
-print('Starting...')
-while True:
-  try:
-    data = adafruit_io.receive(ifttt_feed.key)
-    if data.value == 'wakeup':
-      acknowledge_adafruit_event()
-      for light in lights:
-        light.on = True
-        light.brightness = 254
-        light.xy = [random.random(), random.random()]
+def disconnected(client):
+    print('Disconnected from Adafruit IO')
+
+
+def message_received(client, feed_id, payload):
+    print('Received new value: {0}'.format(payload))
+    if payload == 'wake up':
+        for light in lights:
+            light.on = True
+            light.brightness = 254
+            light.xy = [random.random(), random.random()]
       
-    elif data.value == 'sleep':
-        acknowledge_adafruit_event()
+    elif payload == 'sleep':
         for light in lights:
             light.on = False 
     
-    # Sleep to reduce overload on Adafruit IO service
-    time.sleep(0.5)
+
+print('Starting...')
+
+adafruit_io_client = Adafruit_IO.MQTTClient(config['adafruit_io']['username'], config['adafruit_io']['key'])
+
+# Setup callback functions
+adafruit_io_client.on_connect = connected
+adafruit_io_client.on_disconnect = disconnected
+adafruit_io_client.on_message = message_received
+
+adafruit_io_client.connect()
+
+adafruit_io_client.loop_background()
+
+while True:
+  try:
+      time.sleep(100)
 
   except KeyboardInterrupt:
     print('Quitting')
