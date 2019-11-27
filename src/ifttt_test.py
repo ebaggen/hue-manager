0311013146b89bf1cc7724a6c988c3f6c297ad85
Eric Baggen <eric.baggen@gmail.com>
2019-11-27 14:16:55 -0800

    Functionality complete

--- src/ifttt_test.py
+++ src/ifttt_test.py
@@ -3,12 +3,19 @@ import random
 import time
 import Adafruit_IO
 import toml
+import asyncio
+import numpy
+import csv
+import math
 
 # Import configuration file
 config = toml.load('config.toml')
 
 # Connect to Adafruit IOT services and IFTTT feed
 adafruit_io = Adafruit_IO.Client(config['adafruit_io']['username'], config['adafruit_io']['key'])
+
+cct_lookup = {}
+
 try:
     ifttt_feed = adafruit_io.feeds('ifttt')
 except Adafruit_IO.RequestError:
@@ -20,43 +27,54 @@ bridge = phue.Bridge(config['hue']['ip_address'])
 lights = bridge.get_light_objects()
 
 def connected(client):
-    print('Connected to Adafruit IO.')
     client.subscribe(config['adafruit_io']['feed_id'], config['adafruit_io']['username'])
 
-def disconnected(client):
-    print('Disconnected from Adafruit IO')
-
+def wake_up_sequence():
+    now = time.time()
+    sequence_duration = config['wake_up_sequence']['duration'] * 60 # in seconds
+    end_time = now + sequence_duration
+    remaining_time = sequence_duration
+    while remaining_time > 0:
+        remaining_time = (end_time - time.time())
+        percent_complete = 100 - 100*(remaining_time / sequence_duration)
+        for light in lights:
+            light.on = True
+            light.brightness = max(min(int(percent_complete / 100 * 254), 254), 0)
+            low_temperature = config['wake_up_sequence']['starting_temperature']
+            high_temperature = config['wake_up_sequence']['end_temperature']
+            desired_temperature = low_temperature + (high_temperature - low_temperature) * percent_complete/100
+            rounded_temperature = min(cct_lookup, key=lambda x:abs(float(x) - desired_temperature))
+            light.xy = cct_lookup[rounded_temperature]
+        time.sleep(1)
 
 def message_received(client, feed_id, payload):
     print('Received new value: {0}'.format(payload))
     if payload == 'wake up':
-        for light in lights:
-            light.on = True
-            light.brightness = 254
-            light.xy = [random.random(), random.random()]
-      
+        wake_up_sequence()
     elif payload == 'sleep':
         for light in lights:
             light.on = False 
+
+if __name__ == '__main__':    
+    print('Starting...')
     
+    with open ('CCT-lookup-table.txt', mode='r') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        x_bias = config['wake_up_sequence']['x_bias']
+        y_bias = config['wake_up_sequence']['y_bias']
+        for entry in reader:
+            cct_lookup[entry['CCT']] = [float(entry['x (black body)']) + x_bias,
+                                        float(entry['y (black body)']) + y_bias]
 
-print('Starting...')
+    adafruit_io_client = Adafruit_IO.MQTTClient(config['adafruit_io']['username'], config['adafruit_io']['key'])
 
-adafruit_io_client = Adafruit_IO.MQTTClient(config['adafruit_io']['username'], config['adafruit_io']['key'])
+    # Setup callback functions
+    adafruit_io_client.on_connect = connected
+    adafruit_io_client.on_message = message_received
 
-# Setup callback functions
-adafruit_io_client.on_connect = connected
-adafruit_io_client.on_disconnect = disconnected
-adafruit_io_client.on_message = message_received
+    adafruit_io_client.connect()
 
-adafruit_io_client.connect()
-
-adafruit_io_client.loop_background()
-
-while True:
-  try:
-      time.sleep(100)
-
-  except KeyboardInterrupt:
-    print('Quitting')
-    break
+    adafruit_io_client.loop_background()
+    
+    while True:
+        time.sleep(100)
